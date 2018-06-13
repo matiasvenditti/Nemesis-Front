9ca2df0df4c4e4daecce84c2fd5fc52d45f2c21f
Matias <matias.venditti@ing.austral.edu.ar>
2018-06-13 00:57:35 -0300

    store home images

--- src/app/home/store-result/store-result.component.ts
+++ src/app/home/store-result/store-result.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Store } from '../../../model/store';
+import { StoreService } from '../../../services/store.service';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Image } from '../../../model/image';
 
 @Component({
   selector: 'app-store-result',
@@ -10,8 +13,12 @@ export class StoreResultComponent implements OnInit {
 
   @Input() store:Store;
 
-  constructor() {}
+  constructor(private storeService: StoreService, private sanitizer: DomSanitizer) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.storeService.getStoreImage(this.store.id).subscribe((res: Image) => {
+      this.store.imageUrl = this.sanitizer.bypassSecurityTrustUrl('data:image/jpg;base64,' + res.code);
+    })
+  }
 
 }
